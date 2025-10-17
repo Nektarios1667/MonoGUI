5064dd1962ad443840311a005aa9873b6be80ff4
Nektarios1667 <nektarioslavdas@gmail.com>
2025-10-17 12:15:05 -0400

    Group widget added

---
+++ Widgets/Group.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGUI.Widgets;
+public class Group : Widget
+{
+    public List<Widget> Widgets { get; private set; } = new List<Widget>();
+    public Group(GUI gui) : base(gui, Point.Zero) { }
+    public override void Update()
+    {
+        if (!Visible) return;
+        foreach (var widget in Widgets) widget.Update();
+    }
+    public override void Draw()
+    {
+        if (!Visible) return;
+        foreach (var widget in Widgets) widget.Draw();
+    }
+    public void AddWidget(Widget widget) { Widgets.Add(widget); }
+    public void AddWidgets(params Widget[] widgets) { Widgets.AddRange(widgets); }
+    public void RemoveWidget(Widget widget) { Widgets.Remove(widget); }
+    public void RemoveWidget(int idx) { Widgets.RemoveAt(idx); }
+}
