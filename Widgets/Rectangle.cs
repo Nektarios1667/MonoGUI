1d57340884d42ebe2d91977ac41978e8b45202e2
Nektarios Lavdas <nektarioslavdas@gmail.com>
2026-07-26 17:48:25 -0400

    Rectangle widget

---
+++ Widgets/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGUI.Widgets;
+
+public class Rectangle : Widget
+{
+    public Microsoft.Xna.Framework.Rectangle Rect { get; set; }
+    public Color FillColor { get; set; }
+    public Color BorderColor { get; set; }
+    public int BorderThickness { get; set; }
+    public Rectangle(GUI gui, Point location, Point size, Color fill, Color border, int borderThickness) : base(gui, location)
+    {
+        Rect = new(location, size);
+        FillColor = fill;
+        BorderColor = border;
+        BorderThickness = borderThickness;
+    }
+    public override void Update() { }
+    public override void Draw()
+    {
+        Gui.Batch.FillRectangle(Rect, FillColor);
+        Gui.Batch.DrawRectangle(Rect, BorderColor, BorderThickness);
+    }
+}
